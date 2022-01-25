03726d8f668a34da340e90a3d954577b8baddf83
Anthony <adominguez15722@gmail.com>
2022-01-25 15:08:26 -0600

    Final Submission

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const cors = require('cors')
+const {getHouses, deleteHouse, createHouse, updateHouse} = require('./controller')
 const app = express()
+
+
+
+
 
 app.use(cors())
 app.use(express.json())
+
+
+app.get('/api/houses', getHouses)
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+app.delete('/api/houses/:id', deleteHouse)
 
 
 app.listen(4004, () => console.log('Server running on 4004'))
